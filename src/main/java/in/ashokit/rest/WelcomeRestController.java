470b5b95f764b698137adbc1883fdfdea46f170e
sha02shi92 <gajbhiyes0@gmail.com>
2022-11-13 12:54:15 +0530

    this is my security code

---
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package in.ashokit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return " welcome to shashank gajbhiye white house..!!";
+	}
+
+}
